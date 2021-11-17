fb6b05db9f2df8a1ae0709a16e588ff3ef3140fd
Ben Vandenberg <36287081+BenPVandenberg@users.noreply.github.com>
2021-11-17 11:42:16 -0500

    Keep blackjack values out of Deck + Card objects

--- bellagio/deck.py
+++ bellagio/deck.py
@@ -9,10 +9,10 @@ class Deck:
 
     def build(self):
         for suit in Card.SUITS:
-            for rank in range(2, 11):
-                self.cards.append(Card(suit, rank))
+            for rank in range(1, 11):
+                self.cards.append(Card(rank, suit))
             for face_card in Card.FACE_CARDS:
-                self.cards.append(Card(suit, face_card))
+                self.cards.append(Card(face_card, suit))
 
     def shuffle(self):
         random.shuffle(self.cards)
@@ -28,21 +28,17 @@ class Card:
     SUITS = ["Clubs", "Diamonds", "Hearts", "Spades"]
     FACE_CARDS = ["Jack", "Queen", "King"]
 
-    def __init__(self, suit, rank):
+    def __init__(self, rank, suit):
         self.suit = suit
         self.rank = rank
+
+        if self.rank in ["Jack", "Queen", "King"]:
+            self.value = 10
+        else:
+            self.value = int(self.rank)
 
     def __repr__(self):
         return str(self.rank) + " of " + self.suit
 
     def __eq__(self, other):
         return self.rank == other.rank and self.suit == other.suit
-
-    def get_value(self):
-        if self.rank in ["Jack", "Queen", "King"]:
-            return 10
-        # Ace is 1 or 11
-        elif self.rank == 1:
-            return [1, 11]
-        else:
-            return int(self.rank)
